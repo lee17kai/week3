a9f6d938c3c29b01364f92b9313c73d6aa58fafe
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-04-13 09:40:19 -0500

    0940

--- practice/2-conditionals.js
+++ practice/2-conditionals.js
@@ -12,3 +12,27 @@ function getRandomInt(max) {
 //   - If the total is anything else, the "point" is set, e.g. "the point is 6"
 
 // Write the recipe (algorithm) in the comments. Write the code.
+
+//Store the total
+let die1 = getRandomInt(10)
+console.log(`Roll 1 is ${die1}`)
+
+let die2 = getRandomInt(10)
+console.log(`Roll 2 is ${die2}`)
+
+let total = die1 + die2
+
+// Say you win if total is 7 or 11
+if(total == 7 || total == 11) {
+  console.log(`The total is ${total}. YOU WIN!!!!`)
+}
+
+//Say you lose if total is 2, 3, or 12
+else if(total == 2 || total == 3 || total == 12) {
+  console.log(`The total is ${total}. YOU LOSE :(`)
+}
+
+//Say the point is based on whatever it is
+else{
+  console.log(`The point is ${total}`)
+}
